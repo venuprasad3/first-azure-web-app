8bcc354cc00187dfcf111a243a7a561c02bfc077
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 12:58:31 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set("allow_url_fopen", 1);
+$json = file_get_contents('https://login.microsoftonline.com/common/oauth2/v2.0/token');
+$obj = json_decode($json);
+echo $obj->access_token;
+
 echo "Hello World!";
 
 ?>
