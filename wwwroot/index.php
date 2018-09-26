102f448673286a4ab435e575f6a182ba7c5b117d
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:39:14 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,5 +1,14 @@
 <?php
 
-$response = file_get_contents('https://management.azure.com/subscriptions/8f8cd043-9361-4936-ab7b-c9eabac3eb62/providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=2015-01-01');
-echo $response;
+try{
+    $subscription = Get-AzureRmSubscription
+}
+catch{
+    Login-AzureRmAccount
+    $subscription = Get-AzureRmSubscription
+}
+
+$tenantId = $subscription.TenantId
+echo $tenantId;
+
 ?>
