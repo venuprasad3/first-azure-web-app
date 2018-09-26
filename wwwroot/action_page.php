15387ec44a5e6c8d17be546f9c0680218d268883
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 16:32:01 +0530

    Update action_page.php

--- wwwroot/action_page.php
+++ wwwroot/action_page.php
@@ -1,9 +1,22 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["clientID"]; ?><br>
- <?php echo $_GET["clientSecret"]; ?><br>
-  <?php echo $_GET["tenant"]; ?><br>
+<?php $clientID = $_GET["clientID"]; ?><br>
+<?php $clientSecret = $_GET["clientSecret"]; ?><br>
+<?php $tenant = $_GET["tenant"]; ?><br>
+
+<?php
+$resAzureGraphAPI = "https://graph.windows.net";
+$serviceRootURL = "https://graph.windows.net/$tenant"
+$authString = "https://login.windows.net/$tenant";
+
+$AuthContext = [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext]$authString
+$clientCredential = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential($clientID, $clientSecret)
+
+$authenticationTask = $AuthContext.AcquireTokenAsync($resAzureGraphAPI, $clientCredential);
+$authenticationTask.Wait()
+$authenticationResult = $authenticationTask.Result
+?>
 
 </body>
 </html>
