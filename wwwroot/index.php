0780d71131fc26c327de75ec4d3aedd2a3c8258e
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:20:28 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,10 +1,5 @@
 <?php
 
-ini_set("allow_url_fopen", 1);
-$json = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452');
-$obj = json_decode($json);
-echo $obj->access_token;
-echo "Hello Azure";
-echo "Hello World!";
-
+$response = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452');
+echo $response;
 ?>
