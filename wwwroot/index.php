192961e8a819af5830b845a995ef37d91332a627
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:42:12 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,14 +1,5 @@
 <?php
 
-try{
-    $subscription = Get-AzureRmSubscription
-}
-catch{
-    Login-AzureRmAccount
-    $subscription = Get-AzureRmSubscription
-}
-
-$tenantId = $subscription.TenantId
-echo $tenantId;
+echo "Hello Azure!";
 
 ?>
