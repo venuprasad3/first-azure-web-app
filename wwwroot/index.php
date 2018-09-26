f7a08cd7ba9ea9cec5877fbcd240de0ca9e93ee7
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 15:54:15 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -72,18 +72,18 @@ a {
 
 <form action="/action_page.php">
   <div class="container">
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
+    <h1>Azure Resource Health Register</h1>
+    <p>Please fill in this form.</p>
     <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+    <label for="clientID"><b>Client ID</b></label>
+    <input type="text" placeholder="Enter client ID" name="clientID" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <label for="clientSecret"><b>Client Secret</b></label>
+    <input type="text" placeholder="Enter client Secret" name="clientSecret" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+    <label for="tenant"><b>Tenant</b></label>
+    <input type="text" placeholder="Enter tenant" name="tenant" required>
     <hr>
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
