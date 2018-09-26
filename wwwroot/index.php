e801578fdfb1f97f9ffedb15c25a7d9b16e5a255
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:14:42 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set("allow_url_fopen", 1);
-$json = file_get_contents('https://management.azure.com/subscriptions/8f8cd043-9361-4936-ab7b-c9eabac3eb62/providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=2015-01-01');
+$json = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452');
 $obj = json_decode($json);
 echo $obj->access_token;
 echo "Hello Azure";
