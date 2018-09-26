1778294e32057f4a289e8c3cd78aa994fb20aca2
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:03:01 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -4,7 +4,7 @@ ini_set("allow_url_fopen", 1);
 $json = file_get_contents('https://login.microsoftonline.com/common/oauth2/v2.0/token');
 $obj = json_decode($json);
 echo $obj->access_token;
-
+echo "Hello Azure";
 echo "Hello World!";
 
 ?>
