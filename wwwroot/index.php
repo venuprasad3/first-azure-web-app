1cac46c4df4f5a4bd768305d43e24ee53b000e14
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:21:23 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,5 +1,5 @@
 <?php
 
-$response = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452');
+$response = file_get_contents('https://management.azure.com/subscriptions/8f8cd043-9361-4936-ab7b-c9eabac3eb62/providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=2015-01-01');
 echo $response;
 ?>
