024afbd75d2ab6bfd2dfe7a595b7aace6dfa3631
venuprasad naik <vpnaik97@gmail.com>
2018-09-26 13:04:51 +0530

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set("allow_url_fopen", 1);
-$json = file_get_contents('https://login.microsoftonline.com/common/oauth2/v2.0/token');
+$json = file_get_contents('https://management.azure.com/subscriptions/8f8cd043-9361-4936-ab7b-c9eabac3eb62/providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=2015-01-01');
 $obj = json_decode($json);
 echo $obj->access_token;
 echo "Hello Azure";
